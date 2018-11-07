55b2181bc964856e8fc20b598f4c0c93050cd5fc
Michel Gokan <michel.gokan@kau.se>
2018-11-07 20:41:39 +0100

    Fake config.sh

--- config.sh
+++ config.sh
@@ -2,6 +2,14 @@
 
 PLUGIN_NAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd | xargs basename )"
 
+echo "Checking if $PLUGIN_NAME part 2 exists..."
+
+if [ ! -f ./Plugins/$PLUGIN_NAME/config.sh ]; then
+    echo "Part 2 does not exists..."
+    echo "Exiting installation!"
+    exit 0
+fi
+
 echo "Running ./Plugins/$PLUGIN_NAME"
 cd ./Plugins/$PLUGIN_NAME
 source config.sh
